8e5d69416793fb419ef7229c99e011b27ee64a2f
sharvanr <sharvan@live.nl>
2018-11-14 15:25:28 +0100

    ik hou van rood!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Pizzaroo0oooo0</title>
+    <title>Pizzaroo0oomvkjmkjvoo0</title>
 
 </head>
 <body>
